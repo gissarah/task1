109a706a2b43642b1c4f0e15f374eb42b9aea255
gissarah <125769830+gissarah@users.noreply.github.com>
2024-06-20 00:49:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("men").onclick= function(){
+
+if(document.getElementById("list").style.display=="block"){
+  document.getElementById("list").style.display="none"
+} 
+else{
+  document.getElementById("list").style.display="block"
+}
+  
+}
